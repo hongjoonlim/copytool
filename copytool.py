132c79fcfece98a4c11e92baaba39d81ebbdf39e
hongjoonlim <lim.hongjoon@gmail.com>
2024-02-25 17:24:28 +0100

    added new list box in the left section

--- copytool.py
+++ copytool.py
@@ -14,15 +14,18 @@ class DragDropApp(tk.Tk):
         self.right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
         self.left_label = tk.Label(self.left_frame, text="Drop Files Here", bg="lightgray")
-        self.left_label.pack(pady=20)
+        self.left_label.pack(pady=10)
 
-        self.left_frame.bind("<<Drop>>", self.drop)
+        self.listbox = tk.Listbox(self.left_frame)
+        self.listbox.pack(fill=tk.BOTH, expand=True)
 
         self.select_base_button = tk.Button(self.left_frame, text="Select Base Folder", command=self.select_base_folder)
         self.select_base_button.pack(pady=10)
 
         self.base_folder_label = tk.Label(self.left_frame, text="", bg="lightgray")
         self.base_folder_label.pack()
+
+        self.left_frame.bind("<<Drop>>", self.drop)
 
         self.copy_button = tk.Button(self.right_frame, text="Copy", command=self.copy_folder_name)
         self.copy_button.pack(pady=20)
@@ -40,8 +43,10 @@ class DragDropApp(tk.Tk):
         self.left_label.config(text="Dropped")
         files = self.tk.splitlist(self.tk.call('::tk::DND::Dnd_Files', 'list', event.data))
         for file in files:
-            filename = file.split("/")[-1]  # Extracting filename from the path
-            tk.Label(self.left_frame, text=filename).pack()
+            if file.endswith('.zip') or file.endswith('.tar') or file.endswith('.gz') or file.endswith('.rar'):
+                continue
+            folder_name = file.split("/")[-1]  # Extracting folder name from the path
+            self.listbox.insert(tk.END, folder_name)
 
     def copy_folder_name(self):
         if self.destination_path and self.base_folder:
